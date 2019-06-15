736acf8572fa5bc101eb6db902a0475b5ef4991e
Maryam Alam <maryamalam15@gmail.com>
2019-06-15 17:58:38 +0500

    - creating tables, inserting data in tables.

---
+++ create_tables.py
@@ -0,0 +1,42 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def drop_tables(cur, conn):
+    """
+    drop all tables if any exist.
+    """
+    for query in drop_table_queries:
+        print(query)
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    """
+    create tables.
+    """
+    for query in create_table_queries:
+        print(query)
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn_str = "host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())
+    print(conn_str)
+    conn = psycopg2.connect(conn_str)
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
